6b9a6e30b88611954f7c4fe59ee5a2a4654efb7a
wafuwafu13 <mariobaske@i.softbank.jp>
2021-09-13 18:21:15 +0900

    some process...

--- script/test/check.js
+++ script/test/check.js
@@ -1,5 +1,8 @@
 const { exit } = require("shelljs");
 
+if (1 < 2) {
+  console.error("This error is critical");
+  exit(1);
+}
 
-console.error('This error is critical')
-exit(1)
+console.log('some process...')
