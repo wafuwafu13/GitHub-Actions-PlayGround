4a42bbf08cb919219d6348db4b08ec65a59d9741
wafuwafu13 <wafuwafu1313@gmail.com>
2021-12-20 18:22:56 +0900

    ENV_NAME

--- shell/index.sh
+++ shell/index.sh
@@ -1,14 +1,3 @@
-echo "choose yes or no!!!"
+env_name=${ENV_NAME:="develop"}
 
-flag=${FLAG:="FALSE"}
-if [ "${flag}" = "FALSE" ]; then
-  while true; do
-    read -r -p "yes or no: " yn
-    case ${yn} in
-      [Yy]* ) break ;;
-      [Nn]* ) exit 1 ;;
-    esac
-  done
-fi
-
-echo "some process..."
+msg "env_name is ${env_name}"
