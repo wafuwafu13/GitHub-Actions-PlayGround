120c5c1d1d8d2cd757ef06463a1fec886ec2c18e
wafuwafu13 <wafuwafu1313@gmail.com>
2023-01-18 10:51:37 +0900

    gen.sh

--- go/main.go
+++ go/main.go
@@ -3,14 +3,25 @@ package main
 import (
 	"fmt"
 	"os/exec"
-	"os"
+)
+
+const (
+	gen_cert_script = "./gen-cert.sh"
 )
 
 func main() {
-	err := exec.Command("openssl", "genrsa", "-out", "ca.key", "2048").Run()
-	fmt.Println("err: ", err)
+	err := exec.Command("chmod", "777", gen_cert_script).Run()
+
+	if err != nil {
+		fmt.Printf("Failed to chmod: %s", err)
+	}
+
+	err = exec.Command("sh", "-c", gen_cert_script).Run()
+	if err != nil {
+		fmt.Printf("Failed to run: %s", err)
+	}
+
 	out, err := exec.Command("ls").Output()
 	fmt.Printf("%s", out)
-	os.Remove("./ca.key")
 }
 
