e4f5ce0d974fb252b83da9cb3ee574672d91c4e2
wafuwafu13 <wafuwafu1313@gmail.com>
2023-02-01 09:53:04 +0900

    windows adapter

--- go/main.go
+++ go/main.go
@@ -8,8 +8,35 @@ import (
 	"runtime"
 	"syscall"
 	"unsafe"
+	"os"
 	"golang.org/x/sys/windows"
 )
+
+func adapterAddresses() ([]*windows.IpAdapterAddresses, error) {
+	var b []byte
+	l := uint32(15000) // recommended initial size
+	for {
+		b = make([]byte, l)
+		err := windows.GetAdaptersAddresses(syscall.AF_UNSPEC, windows.GAA_FLAG_INCLUDE_PREFIX, 0, (*windows.IpAdapterAddresses)(unsafe.Pointer(&b[0])), &l)
+		if err == nil {
+			if l == 0 {
+				return nil, nil
+			}
+			break
+		}
+		if err.(syscall.Errno) != syscall.ERROR_BUFFER_OVERFLOW {
+			return nil, os.NewSyscallError("getadaptersaddresses", err)
+		}
+		if l <= uint32(len(b)) {
+			return nil, os.NewSyscallError("getadaptersaddresses", err)
+		}
+	}
+	var aas []*windows.IpAdapterAddresses
+	for aa := (*windows.IpAdapterAddresses)(unsafe.Pointer(&b[0])); aa != nil; aa = aa.Next {
+		aas = append(aas, aa)
+	}
+	return aas, nil
+}
 
 func main() {
 	conf, err := dns.ClientConfigFromFile("/etc/resolv.conf")
@@ -20,22 +47,15 @@ func main() {
 	fmt.Println("=========================")
 
 	if runtime.GOOS == "windows" {
-		l := uint32(15000)
-		b := make([]byte, l)
-		err := windows.GetAdaptersAddresses(syscall.AF_UNSPEC, windows.GAA_FLAG_INCLUDE_PREFIX, 0, (*windows.IpAdapterAddresses)(unsafe.Pointer(&b[0])), &l)
-		if err == nil {
-			if l == 0 {
-				fmt.Println("nameserver: can't getadaptersaddresses")
-			}
+		servers, err := adapterAddresses()
+		if err != nil {
+			fmt.Printf("err: %+v \n", err)
 		}
-		fmt.Println(err)
-		var aas []*windows.IpAdapterAddresses
-		for aa := (*windows.IpAdapterAddresses)(unsafe.Pointer(&b[0])); aa != nil; aa = aa.Next {
-			aas = append(aas, aa)
-		}
-		fmt.Printf("%+v \n", aas)
-		for _, v := range aas {
+		for _, v := range servers {
 			fmt.Printf("%+v \n", v.FirstDnsServerAddress.Address.IP().String())
+			// 168.63.129.16 
+      // fec0:0:0:ffff::1 
+      // fec0:0:0:ffff::1 
 		}
 	}
 
