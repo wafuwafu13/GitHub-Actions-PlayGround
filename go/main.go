42a2104434534dd6509422e560fce63b0b578763
wafuwafu13 <wafuwafu1313@gmail.com>
2023-02-01 09:32:55 +0900

    go windows

--- go/main.go
+++ go/main.go
@@ -5,6 +5,10 @@ import (
 	"github.com/miekg/dns"
 	"net"
 	"strconv"
+	"runtime"
+	"syscall"
+	"unsafe"
+	"golang.org/x/sys/windows"
 )
 
 func main() {
@@ -14,6 +18,26 @@ func main() {
 	}
 	fmt.Printf("%+v \n", conf)
 	fmt.Println("=========================")
+
+	if runtime.GOOS == "windows" {
+		l := uint32(15000)
+		b := make([]byte, l)
+		err := windows.GetAdaptersAddresses(syscall.AF_UNSPEC, windows.GAA_FLAG_INCLUDE_PREFIX, 0, (*windows.IpAdapterAddresses)(unsafe.Pointer(&b[0])), &l)
+		if err == nil {
+			if l == 0 {
+				fmt.Println("nameserver: can't getadaptersaddresses")
+			}
+		}
+		fmt.Println(err)
+		var aas []*windows.IpAdapterAddresses
+		for aa := (*windows.IpAdapterAddresses)(unsafe.Pointer(&b[0])); aa != nil; aa = aa.Next {
+			aas = append(aas, aa)
+		}
+		fmt.Printf("%+v \n", aas)
+		for _, v := range aas {
+			fmt.Printf("%+v \n", v.FirstDnsServerAddress.Address.IP().String())
+		}
+	}
 
 	nameserver := conf.Servers[0]
 	nameserver = net.JoinHostPort(nameserver, strconv.Itoa(53))
